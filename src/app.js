a3d5d521474fd50bd63aca1c37918c80926429ef
Leseli <leselintiise@gmail.com>
2024-10-01 08:48:55 +0200

    Added backticks to api URL

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
 
+  let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
   let apiKey = "3afd9f54to3b9d310c00cc77b41ae58c";
-  let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(updateWeather);
 }
 
